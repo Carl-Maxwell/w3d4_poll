d0d0ff1d6b17626faad522ffeee70b5bcb8f3a70
App Academy Student <student@appacademy.io>
2015-07-09 15:00:54 -0700

    working on question#results

--- app/models/question.rb
+++ app/models/question.rb
@@ -23,17 +23,23 @@ class Question < ActiveRecord::Base
   )
 
   def results
-    Question.find_by_sql([<<-SQL, self.id])
-      SELECT
-        answer_choices.*, COUNT(responses.*) AS num_responses
-      FROM
-        answer_choices
-      LEFT OUTER JOIN
-        responses ON answer_choices.id = responses.answer_choice_id
-      WHERE
-        answer_choices.question_id = ?
-      GROUP BY
-        answer_choices.id
-    SQL
+    # Question.find_by_sql([<<-SQL, self.id])
+    #   SELECT
+    #     answer_choices.*, COUNT(responses.*) AS num_responses
+    #   FROM
+    #     answer_choices
+    #   LEFT OUTER JOIN
+    #     responses ON answer_choices.id = responses.answer_choice_id
+    #   WHERE
+    #     answer_choices.question_id = ?
+    #   GROUP BY
+    #     answer_choices.id
+    # SQL
+    results = answer_choices
+      .select("answer_choices.*, COUNT(responses.*) AS num_responses")
+      .joins("LEFT OUTER JOIN responses ON answer_choices.id = responses.answer_choice_id")
+      .group("answer_choices.id")
+
+    results.map { |result| [result.choice, result.num_responses] }
   end
 end
